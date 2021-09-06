e1c4889311a40c41f38fc1d079fe9d4854645c81
JerKaiLee <jleeclimbs@gmail.com>
2021-09-06 11:56:20 -0700

    added exercises 1.6 and 1.7

---
+++ testEOF.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Verifies that the expression getchar() != EOF is 0 or 1 */
+
+main()
+{
+   int c;
+
+   while (c=getchar() != EOF)
+      printf("%d\n", c);
+
+   printf("%d - at EOF\n", c);
+}
